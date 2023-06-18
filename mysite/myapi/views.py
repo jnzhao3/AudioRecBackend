45c2a83f18b9715d1578da64fd2cb177bff076fb
Sushant Belsare <sushantbelsare@Sushants-MacBook-Pro.local>
2023-06-17 17:17:28 -0700

    Initial Setup

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, decorators, response, views, status
 
-# Create your views here.
+
+class RecordingAPIView(views.APIView):
+    
+    # @decorators.action(methods=['get'], detail=True)
+    def get(self, request, pk=None):
+        return response.Response([
+            {
+                "day": "Saturday",
+                "date":"Unix Timestamp",
+                "location":{
+                    "lat": 0.0,
+                    "lng": 0.0
+                },
+                "duration": 2138218937123
+            }
+        ], status=status.HTTP_200_OK)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myapi.apps.MyapiConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +52,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "mysite.urls"
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
