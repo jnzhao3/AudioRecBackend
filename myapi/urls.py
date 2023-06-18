df70fd1d9a18a80aa8ca6c6f90c406cd006deaf8
Jennifer Zhao <jennifer-zhao@berkeley.edu>
2023-06-18 00:59:48 -0700

    Initial Commit

---
+++ myapi/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+
+# router = routers.DefaultRouter()
+# router.register(r'recordings', views.RecordingAPIView.as_view())
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('recordings/', views.RecordingAPIView.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
---
+++ myapi/views.py
@@ -0,0 +1,30 @@
+from rest_framework import viewsets, decorators, response, views, status
+from .forms import UploadFileForm
+from .storage import uploadFile
+
+class RecordingAPIView(views.APIView):
+    
+    # @decorators.action(methods=['get'], detail=True)
+    def get(self, request, pk=None):
+        return response.Response([
+            {
+                "day": "Saturday",
+                "date":"Unix Timestamp",
+                "location":{
+                    "lat": 0.0,
+                    "lng": 0.0
+                },
+                "duration": 2138218937123
+            }
+        ], status=status.HTTP_200_OK)
+    
+    def post(self, request, pk=None):
+        if request.method == "POST":
+            form = UploadFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                file = request.FILES["file"]
+                uploadFile(file)
+                return response.Response("File Uploaded Successfully", status=status.HTTP_200_OK)
+        else:
+            form = UploadFileForm()
+        return response.Response("File Cannot Be Uploaded", status=status.HTTP_400_BAD_REQUEST)
